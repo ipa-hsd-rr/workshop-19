9ed09d8e93007b6cfefb24e451a4c7b515a6ac3e
Ludovic Delval <ludovic.delval@ipa.fraunhofer.de>
2019-05-03 15:15:21 +0200

    check indentation

--- ros2_simple_ws/src/ros2_listener/ros2_listener/scripts/listener.py
+++ ros2_simple_ws/src/ros2_listener/ros2_listener/scripts/listener.py
@@ -5,27 +5,22 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 class ros2_listener(Node):
-        def __init__(self):
-                super().__init__('ros2_listener')
-                self.sub = self.create_subscription(String, 'ros2_topic', self.callback)
+    def __init__(self):
+        super().__init__('ros2_listener')
+        self.sub = self.create_subscription(String, 'subject_topic', self.callback)
 
-        def callback(self, message):
-                self.get_logger().info( "ROS_2 listener listening to %s"% message.data)
+    def callback(self, message):
+        self.get_logger().info( "ROS_2 listener listening to %s"% message.data)
 
 def main():
-        rclpy.init()
-        node = ros2_listener()         #create node
-        
-        rclpy.spin(node)
+    rclpy.init()
+    node = ros2_listener()
 
-        node.destroy_node()     #destructor
-        rclpy.shutdown()        #shutdown node
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
-   
-
-
-
-			
--- ros2_simple_ws/src/ros2_talker/ros2_talker/scripts/talker.py
+++ ros2_simple_ws/src/ros2_talker/ros2_talker/scripts/talker.py
@@ -2,34 +2,31 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from time import sleep
 
 
 class ros2_talker(Node):
+    def __init__(self):
+        super().__init__('ros2_talker')
+        self.i = 0
+        self.pub = self.create_publisher(String, 'subject_topic')
+        timer_period = 0.1        #Set the publishing rate
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        def __init__(self):
-                super().__init__('ros2_talker')
-                self.i = 0
-                self.pub = self.create_publisher(String, 'ros2_topic')
-                timer_period = 1.0        #Set the publishing rate
-                self.timer = self.create_timer(timer_period, self.timer_callback)
-
-        def timer_callback(self):
-                message = String()
-                self.i += 1
-                message.data = 'ROS_2 talker talking %d'% self.i
-                self.get_logger().info(message.data)
-                self.pub.publish(message) 
+    def timer_callback(self):
+        message = String()
+        self.i += 1
+        message.data = 'ROS_2 talker talking %d'% self.i
+        self.get_logger().info(message.data)
+        self.pub.publish(message) 
 
 
 def main():
-        rclpy.init()
-        node = ros2_talker()         #create node
-        rclpy.spin(node)
-        node.destroy_node()     #destructor
-        rclpy.shutdown()        #shutdown node
+    rclpy.init()
+    node = ros2_talker()         #create node
+    rclpy.spin(node)
+    node.destroy_node()     #destructor
+    rclpy.shutdown()        #shutdown node
 
 
 if __name__ == '__main__':
     main()
-   
